6bbe2a609034b3f7ab04f704ef29f8b2f8f1b0cf
Vasiliy Stepanov <36109277+kshishtanchik@users.noreply.github.com>
2026-03-29 21:26:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,5 @@ self.addEventListener('install', (e) => {
     console.log('Service Worker: Installed');
   });
   
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  });
+self.addEventListener('fetch', (event) => {});
   
